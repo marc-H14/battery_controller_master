e1c72b0b5a4bda23fcf9f4bcb5db15d793d1213e
marc-H14 <marc.p.hofer@gmx.ch>
2022-10-02 18:04:06 +0200

    Added BMS request, Serial data transmission

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -50,8 +50,12 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint8_t bms_update;
 uint16_t temp_in[4];
 uint16_t temp_out[4];
+uint8_t serial_out[154 + 8 + 4];
+uint8_t BMS_REQUEST_DATA[10] = {"\x7E\xA1\x01\x00\x00\xBE\x18\x55\xAA\x55"}; // Requests 144 byte
+uint8_t bms_rx_buffer[154];
 uint16_t TEMP_TABLE[] = {
         1549, 1585, 1621, 1658, 1694, 1730, 1767, 1803, 1840, 1876, 1913, 1949, 1986, 2022, 2059, 2096,
         2132, 2169, 2206, 2242, 2279, 2316, 2353, 2390, 2426, 2463, 2500, 2537, 2574, 2611, 2648, 2685,
@@ -162,12 +166,28 @@ void crc_init(void) {
     CRC->CR |= CRC_CR_RESET;   // Reset calculation
 }
 
+void transmit_serial() {
+    crc_init();
+    uint8_t i;
+    CRC->CR = 1;
+    for (i = 0; i < (154 + 8); i++) {
+        CRC->DR = serial_out[i];
+    }
+    uint32_t crc[1] = {CRC->DR};
+    memcpy(serial_out + 8 + 154, crc, 4);
+    if (!bms_update) memset(serial_out + 8, 0x00, 154);
+    HAL_UART_Transmit(&huart3, serial_out, 154 + 8 + 4, 100);
+    bms_update = 0;
+}
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-   if (huart == &huart3) {
+    if (huart == &huart1) {
+        memcpy(serial_out + 8, bms_rx_buffer, 154);
+        bms_update = 1;
+    } else if (huart == &huart3) {
         HAL_NVIC_SystemReset();
-   }
+    }
 }
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
@@ -188,20 +208,36 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
     if (temp_in[3] > MAX) temp_out[3] = 0xFFFF;
     else if (temp_in[3] < MIN) temp_out[3] = 0x0000;
     else temp_out[3] =  TEMP_TABLE[temp_in[3] - MIN];
-
+    /*float temp_out[4];
+    temp_out[0] =  (float)TEMP_TABLE[temp_in[0] - MIN] / (65535.0f / 210.0f) - 60;
+    temp_out[1] =  (float)TEMP_TABLE[temp_in[1] - MIN] / (65535.0f / 210.0f) - 60;
+    temp_out[2] =  (float)TEMP_TABLE[temp_in[2] - MIN] / (65535.0f / 210.0f) - 60;
+    temp_out[3] =  (float)TEMP_TABLE[temp_in[3] - MIN] / (65535.0f / 210.0f) - 60;*/
 
 
     uint8_t c[70];
-    int l = sprintf(c, "%4d %4d %4d %4d\n", temp_out[0], temp_out[1], temp_out[2], temp_out[3]);
+    int l = sprintf(c, "%4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d %4d\n", ((uint16_t)serial_out[1] << 8) | serial_out[0],
+                    ((uint16_t)serial_out[3] << 8) | serial_out[2],
+                    ((uint16_t)serial_out[5] << 8) | serial_out[4],
+                    ((uint16_t)serial_out[7] << 8) | serial_out[6],
+                    (serial_out[9] << 8) , serial_out[8],
+                    (serial_out[11] << 8) , serial_out[10],
+                    (serial_out[13] << 8) , serial_out[12],
+                    (serial_out[15]) , serial_out[14],
+                    serial_out[16]);
     CDC_Transmit_FS(c, l);
 
 
+    memcpy(serial_out, temp_out, 8);
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+    transmit_serial();
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
 {
     HAL_ADC_Start_DMA(&hadc1, (uint32_t *) temp_in, 4);
+    HAL_UART_Transmit(&huart1, BMS_REQUEST_DATA, 10, 100);
+    HAL_UART_Receive_DMA(&huart1, bms_rx_buffer, 154);
 }
 /* USER CODE END 0 */
 
@@ -211,53 +247,54 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_USART1_UART_Init();
-  MX_ADC1_Init();
-  MX_USART3_UART_Init();
-  MX_USB_DEVICE_Init();
-  MX_TIM2_Init();
-  MX_CRC_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_USART1_UART_Init();
+    MX_ADC1_Init();
+    MX_USART3_UART_Init();
+    MX_USB_DEVICE_Init();
+    MX_TIM2_Init();
+    MX_CRC_Init();
+    /* USER CODE BEGIN 2 */
+    bms_update = 0;
     HAL_ADCEx_Calibration_Start(&hadc1);
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
     HAL_TIM_Base_Start_IT(&htim2);
     HAL_UART_Receive_DMA(&huart3, NULL, 1);
 
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+    {
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
 }
 
 /**
@@ -266,45 +303,45 @@ int main(void)
   */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC|RCC_PERIPHCLK_USB;
+    PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+    PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -317,13 +354,13 @@ void SystemClock_Config(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
